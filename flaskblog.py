3acf5b2e48fbc08048299457a33b47b2cab3c3d3
mitchavery <avery.mitch651@gmail.com>
2020-05-18 21:57:34 -0700

    templates

--- flaskblog.py
+++ flaskblog.py
@@ -1,17 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Mitch Avery', 
+        'title': 'Blog Post 1', 
+        'content': 'First post content', 
+        'date': 'April 2020'
+    }, 
+    {
+        'author': 'Kyle ', 
+        'title': 'Blog Post 2', 
+        'content': 'First post2 content', 
+        'date': 'April2 2020'
+    }
+]
 
 
 @app.route("/")
 @app.route("/home")
 def hello():
-    return "<h1>World!</h1>"
+    return render_template('home.htm', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.htm')
 
 
 if __name__ == "__main__":
