dc60ca2d2b9e6541a95adfd03bbebcbeea8de502
mitchavery <avery.mitch651@gmail.com>
2020-05-19 17:00:14 -0700

    login forms

--- flaskblog.py
+++ flaskblog.py
@@ -1,33 +1,47 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'c204abc7aaf6f02e66d5b6488eda1307'
 
 posts = [
     {
-        'author': 'Mitch Avery', 
-        'title': 'Blog Post 1', 
-        'content': 'First post content', 
-        'date_posted': 'April 2020'
-    }, 
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
     {
-        'author': 'Kyle ', 
-        'title': 'Blog Post 2', 
-        'content': 'First post2 content', 
-        'date_posted': 'April2 2020'
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
     }
 ]
 
 
 @app.route("/")
 @app.route("/home")
-def hello():
+def home():
     return render_template('home.htm', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return render_template('about.htm', title='about')
+    return render_template('about.htm', title='About')
 
 
-if __name__ == "__main__":
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    return render_template('register.htm', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.htm', title='Login', form=form)
+
+
+if __name__ == '__main__':
     app.run(debug=True)
